4ddbc7d1f685e3d9d3845ecd28357175a2ba741e
Chandan-N-2004 <chandan2004.n@gmail.com>
2026-02-12 01:05:54 +0530

    added create and delete off events

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -7,3 +7,25 @@ router.get("/", eventController.getEvents);
 router.delete("/:id", eventController.deleteEvent);
 
 module.exports = router;
+
+// Create event
+router.post("/", async (req, res) => {
+  try {
+    const event = new Event(req.body);
+    await event.save();
+    res.json(event);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// Delete event (Admin)
+router.delete("/:id", async (req, res) => {
+  try {
+    await Event.findByIdAndDelete(req.params.id);
+    res.json({ message: "Event deleted" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
